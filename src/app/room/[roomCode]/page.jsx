179ef6c7f656d784552d1a9b31f96a829608b9a2
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2024-11-10 22:32:58 +0200

    room styling done

--- src/app/room/[roomCode]/page.jsx
+++ src/app/room/[roomCode]/page.jsx
@@ -164,11 +164,11 @@ export default function ChatLayout({ params }) {
 
   return (
     <div className="flex flex-col h-100">
-      <div className="flex-grow p-6 overflow-y-auto">
+      <div className="flex-grow room-message-container">
         {messages.length === 0 ? (
           <p>No messages yet...</p>
         ) : (
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col">
             {messages.map((message, index) => {
               // Ensure roomData.players exists before accessing
               const player = roomData?.players?.find(player => player.userId === message.userId);
@@ -179,20 +179,14 @@ export default function ChatLayout({ params }) {
                 : `${characterName} (${message.username || 'Unknown User'})`;
 
               return (
-                <div
-                  key={index}
-                  className={`p-4 rounded-lg ${message.role === 'user' ? 'bg-blue-500 text-white self-end' : 'bg-gray-800 self-start'}`}
-                >
+                <div key={index} className={`${message.role === 'user' ? 'player-message self-end' : 'bot-message self-start'}`}>
                   <strong>{displayName}</strong>
                   <div
                     dangerouslySetInnerHTML={{ __html: md.render(message.content) }}
                     className="markdown-content"
                   />
                   {message.audioContent && message.role === 'system' && (
-                    <button
-                      onClick={() => playAudio(message.audioContent)}
-                      className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-lg"
-                    >
+                    <button onClick={() => playAudio(message.audioContent)} className="audio-button">
                       Play Audio
                     </button>
                   )}
@@ -203,15 +197,15 @@ export default function ChatLayout({ params }) {
         )}
       </div>
 
-      <form className="p-4 flex" onSubmit={handleSendMessage}>
+      <form className="pt-4 flex" onSubmit={handleSendMessage}>
         <input
-          className="flex-grow p-2 border text-black rounded-lg focus:outline-none focus:ring focus:ring-blue-400"
+          className="flex-grow room-message-input"
           type="text"
           value={input}
           placeholder="Type your message..."
           onChange={(e) => setInput(e.target.value)}
         />
-        <button type="submit" className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-lg">
+        <button type="submit" className="room-send-button">
           Send
         </button>
       </form>
